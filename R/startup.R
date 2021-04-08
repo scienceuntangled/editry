3c11cf6a055242f5536ba9e7acc5d5b5aaab6330
Ben Raymond <ben@theraymonds.org>
2021-04-08 17:46:39 +1000

    startup msg

---
+++ R/startup.R
@@ -0,0 +1,21 @@
+.onAttach <- function(libname, pkgname) {
+    ## some checks
+    node_installed <- !is.null(nr_node_exe())
+    editly_installed <- !is.null(er_editly_exe())
+    needs_install <- c(if (!node_installed) "node", if (!editly_installed) "editly")
+    if (length(needs_install) > 0) {
+        msg <- paste0(paste(needs_install, collapse = " and "), " need", if (length(needs_install) == 1) "s", " to be installed before editry can be used.")
+        if (!node_installed) {
+            if (get_os() %in% c("osx", "linux") && !requireNamespace("archive", quietly = TRUE)) {
+                msg <- c(msg, "the 'archive' package is required to install node: first install 'archive' with")
+                if (!requireNamespace("remotes", quietly = TRUE)) msg <- c(msg, "  install.packages(\"remotes\")")
+                msg <- c(msg, "  remotes::install_github(\"jimhester/archive\")", "then")
+            }
+            msg <- c(msg, "Install node with:", "  nr_install_node()")
+        }
+        if (!editly_installed) {
+            msg <- c(msg, "Install editly with:", "  er_install_editly()")
+        }
+        packageStartupMessage(paste(msg, collapse = "\n"))
+    }
+}
