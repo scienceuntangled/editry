b2028119b893c15ec3ce0e4e3354be43e6b864f9
Ben Raymond <ben@theraymonds.org>
2021-04-10 09:50:53 +1000

    linux support for install_ffmpeg v0.0.5

--- R/ffmpeg.R
+++ R/ffmpeg.R
@@ -1,9 +1,10 @@
 #' Install ffmpeg
 #'
-#' This is a helper function to install ffmpeg. Currently it only works on Windows platforms. The ffmpeg bundle will be downloaded from <https://www.gyan.dev/ffmpeg/builds/> and saved to your user appdata directory.
+#' This is a helper function to install ffmpeg. Currently it only works on Windows and Linux platforms. The ffmpeg bundle will be downloaded from <https://www.gyan.dev/ffmpeg/builds/> (Windows) or <https://johnvansickle.com/ffmpeg/> (Linux) and saved to your user appdata directory.
 #'
-#' @references <https://www.gyan.dev/ffmpeg/builds/>
+#' @references <https://www.gyan.dev/ffmpeg/builds/> <https://johnvansickle.com/ffmpeg/>
 #' @param force logical: force reinstallation if ffmpeg already exists
+#' @param bits integer: 32 or 64, for 32- or 64-bit install. If missing or `NULL`, will be guessed based on `.Machine$sizeof.pointer`
 #' @param check_hash logical: don't check the hash of the downloaded file
 #'
 #' @return the path to the installed executable
@@ -14,12 +15,16 @@
 #' }
 #'
 #' @export
-er_install_ffmpeg <- function(force = FALSE, check_hash = TRUE) {
+er_install_ffmpeg <- function(force = FALSE, bits, check_hash = TRUE) {
     assert_that(is.flag(force), !is.na(force))
     assert_that(is.flag(check_hash), !is.na(check_hash))
+    if (missing(bits) || is.null(bits)) {
+        bits <- tryCatch(if (.Machine$sizeof.pointer==8) 64 else 32, error = function(e) 64)
+    }
+    assert_that(bits %in% c(32, 64))
     my_os <- get_os()
-    if (my_os != "windows" || !isTRUE(.Machine$sizeof.pointer == 8)) {
-        stop("er_install_ffmpeg only supports 64-bit windows platforms. You will need to install ffmpeg yourself and ensure that it is on the system path.")
+    if (!((my_os == "windows" && bits == 64) || my_os == "linux")) {
+        stop("er_install_ffmpeg only supports linux or 64-bit windows platforms. You will need to install ffmpeg yourself and ensure that it is on the system path.")
     }
     existing_exe <- er_ffmpeg_exe()
     path <- file.path(ovideo_app_dir(), "ffmpeg")
@@ -42,9 +47,20 @@ er_install_ffmpeg <- function(force = FALSE, check_hash = TRUE) {
     }
     if (!dir.exists(path)) dir.create(path, recursive = TRUE)
     if (!dir.exists(path)) stop("could not create directory ", path, " for ffmpeg")
-    zipname <- file.path(path, "ffmpeg-git-essentials.7z")
-    dl_url <- "https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-essentials.7z"
-    hash_url <- "https://www.gyan.dev/ffmpeg/builds/sha256-git-essentials"
+
+    if (my_os == "windows") {
+        dl_url <- "https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-essentials.7z"
+        hash_url <- "https://www.gyan.dev/ffmpeg/builds/sha256-git-essentials"
+        hash_algo <- "sha256"
+    } else if (my_os == "linux") {
+        dl_url <- if (bits == 64) "https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz" else "https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-i686-static.tar.xz"
+        hash_url <- paste0(dl_url, ".md5")
+        hash_algo <- "md5"
+    } else {
+        ## should not get here, but just in case
+        stop("unsupported os")
+    }
+    zipname <- file.path(path, basename(dl_url))
     err <- utils::download.file(dl_url, destfile = zipname, mode = "wb")
     if (!err) {
         if (check_hash) {
@@ -52,8 +68,9 @@ er_install_ffmpeg <- function(force = FALSE, check_hash = TRUE) {
             if (is.null(expected_hash)) {
                 warning("could not download the file hash for checking, but proceeding anyway")
             } else {
-                if (!identical(expected_hash, digest::digest(zipname, algo = "sha256", file = TRUE)))
-                    stop("the sha-256 hash of the downloaded file does not match the expected value. The downloaded file might be incomplete or compromised. If you wish to proceed anyway, run this function again with `check_hash = FALSE`")
+                if (grepl("[[:space:]]", expected_hash)) expected_hash <- sub("[[:space:]].*", "", expected_hash)
+                if (!identical(expected_hash, digest::digest(zipname, algo = hash_algo, file = TRUE)))
+                    stop("the hash of the downloaded file does not match the expected value. The downloaded file might be incomplete or compromised. If you wish to proceed anyway, run this function again with `check_hash = FALSE`")
             }
         }
         archive::archive_extract(zipname, dir = path)
@@ -96,7 +113,7 @@ er_ffmpeg_exe <- function() {
 er_add_ffmpeg_path <- function() {
     if (!nzchar(Sys.which("ffmpeg"))) {
         ffmpeg_dir <- dirname(er_ffmpeg_exe())
-        if (!nzchar(ffmpeg_dir)) {
+        if (nzchar(ffmpeg_dir)) {
             psep <- if (get_os() == "windows") ";" else ":"
             Sys.setenv(PATH = paste0(ffmpeg_dir, psep, Sys.getenv("PATH")))
         }
