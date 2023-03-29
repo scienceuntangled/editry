6501b1ee36245ec95d47eff9421addaee1511d1a
Ben Raymond <ben@theraymonds.org>
2023-03-29 11:47:43 +1100

    don't issue startup warnings if docker exists

--- R/startup.R
+++ R/startup.R
@@ -1,10 +1,13 @@
 .onAttach <- function(libname, pkgname) {
     ## some checks
-    node_installed <- !is.null(nr_node_exe())
-    editly_installed <- !is.null(er_editly_exe())
-    needs_install <- c(if (!node_installed) "node", if (!editly_installed) "editly")
-    if (length(needs_install) > 0) {
-        msg <- c(paste0(paste(needs_install, collapse = " and "), " need", if (length(needs_install) == 1) "s", " to be installed before editry can be used. Run:"), "  er_install_editly()")
-        packageStartupMessage(paste(msg, collapse = "\n"))
+    if (!nzchar(Sys.which("docker"))) {
+        ## if docker exists, could be using editly via that, so don't warn
+        node_installed <- !is.null(nr_node_exe())
+        editly_installed <- !is.null(er_editly_exe())
+        needs_install <- c(if (!node_installed) "node", if (!editly_installed) "editly")
+        if (length(needs_install) > 0) {
+            msg <- c(paste0(paste(needs_install, collapse = " and "), " need", if (length(needs_install) == 1) "s", " to be installed before editry can be used. Run:"), "  er_install_editly()")
+            packageStartupMessage(paste(msg, collapse = "\n"))
+        }
     }
 }
